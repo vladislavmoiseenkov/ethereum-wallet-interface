b9b47f1b7ceecd91a7251d68fd55ca7194c39680
vladislavmoiseenkov <vladislavmoiseenkov@gmail.com>
2023-10-28 13:14:48 +0300

    added: chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 import { WagmiConfig } from 'wagmi';
 import { config } from '../src/web3';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig config={config}>
-      <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </WagmiConfig>
   );
 }
